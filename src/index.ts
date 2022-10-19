0e32214b9688f87d157db77a4e4cf2fe7e454cfa
thehungrycoder <rajuru@gmail.com>
2022-10-19 08:13:06 -0400

    add export to main module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { getConfigByISOName, getCountryByISOName } from './countries';
-export { getCalcMethodByName, getCalcMethods, getCalcMethodsByCountry, getJuristicMethods } from './calc-methods';
+export {
+  getCalcMethodByName,
+  getCalcMethods,
+  getCalcMethodsByCountry,
+  getJuristicMethods,
+} from './calc-methods';
 export { CalculationMethod } from './calc-methods/methods';
+export { getDSTStart, getDSTEnd } from './daylight-savings';
