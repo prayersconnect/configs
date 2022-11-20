5a08378c99918c6f4b5fd60bd891b6ba4da55873
thehungrycoder <rajuru@gmail.com>
2022-11-20 15:01:05 -0500

    new zealand dst boundaries and features enablement (#17)
    * new zealand dst boundaries and features enablement
    
    * add range helpers

--- src/daylight-savings/range-helpers.test.ts
+++ src/daylight-savings/range-helpers.test.ts
@@ -29,14 +29,19 @@ describe('quick range helpers', () => {
       getRangeTestObject(2022, 'United States', 'America/New_York'),
       getRangeTestObject(2022, 'United Kingdom', 'Europe/London'),
       getRangeTestObject(2022, 'Canada', 'America/Toronto'),
+      getRangeTestObject(2022, 'New Zealand', 'Pacific/Auckland'),
+
       getRangeTestObject(2023, 'Australia', 'Australia/Sydney'),
       getRangeTestObject(2023, 'United States', 'America/New_York'),
       getRangeTestObject(2023, 'United Kingdom', 'Europe/London'),
       getRangeTestObject(2023, 'Canada', 'America/Toronto'),
+      getRangeTestObject(2023, 'New Zealand', 'Pacific/Auckland'),
+
       getRangeTestObject(2024, 'Australia', 'Australia/Sydney'),
       getRangeTestObject(2024, 'United States', 'America/New_York'),
       getRangeTestObject(2024, 'United Kingdom', 'Europe/London'),
       getRangeTestObject(2024, 'Canada', 'America/Toronto'),
+      getRangeTestObject(2024, 'New Zealand', 'Pacific/Auckland'),
     ]
   );
 });
