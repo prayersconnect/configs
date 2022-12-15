04673ccadeb2dc7d4ab53d195c31c8bf18f3a51b
thehungrycoder <rajuru@gmail.com>
2022-12-14 22:54:22 -0500

    allow getting country by (short)name (#21)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 export {
   getConfigByISOName,
   getCountryByISOName,
+  getCountryByName,
   hasFeature,
   getCountryAlpha2CodesByFeature,
 } from './countries';
+
 export {
   getCalcMethodByName,
   getCalcMethods,
   getCalcMethodsByCountry,
   getJuristicMethods,
 } from './calc-methods';
+
 export { CalculationMethod } from './calc-methods/methods';
 export { getDSTStart, getDSTEnd } from './daylight-savings/index';
 export { getRangesForYear } from './daylight-savings/range-helpers';
