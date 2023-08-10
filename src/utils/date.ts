f8724b1560bcf70954dc78b6875c57e7796f10c0
Ashraf Ali <ashrafali0148@gmail.com>
2023-08-10 08:02:10 +0100

    Initial implementation

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,10 @@
-export function isRamadan(date: Date) {
-  return getHijriMonth(date) === '9';
+import { DateTime } from 'luxon';
+
+export function isRamadan(date: DateTime): boolean {
+  return getHijriMonth(date) === 'Ramadan';
 }
 
-function getHijriMonth(date: Date): string {
+function getHijriMonth(date: DateTime): string {
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
@@ -10,7 +12,7 @@ function getHijriMonth(date: Date): string {
   };
 
   const format = new Intl.DateTimeFormat('en-u-ca-islamic-nu-latn', options);
-  const parts = format.formatToParts(date);
+  const parts = format.formatToParts(date.toJSDate());
 
   // Extract the month from the formatted parts
   const monthPart = parts.find((part) => part.type === 'month');
